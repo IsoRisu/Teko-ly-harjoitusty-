c043354141d0cac1990cbfbdaf31371a7959d8a4
Risu <kraastian2@gmail.com>
2026-09-19 17:52:20 +0300

    aloin työstämään minimax, vielä kesken

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import random
+import copy
 
+PLY = 1
+computed_states = set()
+def bestmove(state, player, opponent):
+    
+
+def minimax(state, ply, player, opponent, available): # available is a list from 0 to 6 where a position is removed when it is full.
+    if PLY == 0 or sum(state) == 42:
+        return
+    for n in available:
+            newstate = copy.deepcopy(state[n]) 
+            newstate[n] += 1
+            computed_states.add(minimax(newstate, ply-1, opponent,  player))
+            if player:
+                
+        
+
+def heuristic(state):
+    return score
 
 if __name__ == "__main__":
     count = [0] * 7
     available = list(range(0,7))
+    turn = True # True for the starting player False for the responding player
 
 
     while True: # The game platform loop
@@ -26,12 +46,12 @@ if __name__ == "__main__":
                     count[choice] += 1
                     print(f"MOVE:{choice}")
 
-            case "MOVE": # MOVE:{int} THe play command
+            case "MOVE": # MOVE:{int} The play command
                 op_move = data  
                 count[int(op_move)] += 1   
                 print(data)
 
-            case "BOARD": # BOARD. Tämä tarkoittaa, että tekoälysi sovelluslogiikan pitäisi asettaa lauta johonkin tiettyyn konfiguraatioon. 
+            case "BOARD": # BOARD. This is the board set command. It first resets the board and then sets it to a configuration using inputs given as a list of moves. 
                 count = [0] * 7
                 if len(data) > 0:
                     for i in data.split(","):
@@ -44,3 +64,5 @@ if __name__ == "__main__":
             case _:
                 print("MOVE: -1")
                 print("Unrecognized tag!")
+
+        not turn
